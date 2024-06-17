2c0d7fa62428c0f5d0510dcb288eb0076c92917b
I0-o-0I <perchik054@gmail.com>
2024-06-17 12:21:25 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+completed_work = 12
+watch = 1.5
+name = "Python"
+time_for_one_task = watch/completed_work
+print("Курс: ", name, ", ", "всего задач: ", completed_work, ", " "затрачено часов: ", watch, ", ",
+      "среденее время выполнения: ", time_for_one_task, " часа.", sep="")
